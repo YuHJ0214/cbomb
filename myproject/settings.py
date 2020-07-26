cb146e7db83a651bb9da15b98dbadd06731d9f03
유형종 <yuhj@yuhyeongjong-ui-MacBookPro-2.local>
2020-07-27 02:01:33 +0900

    nginx configuration update

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qtki+^rxgd8@)#jrq!o$147@0bnxopuakno4okp6)f(vd@lkm+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.8", "127.0.0.1", "cbomb.co.kr"]
+ALLOWED_HOSTS = ["192.168.1.8", "127.0.0.1", ".cbomb.co.kr"]
 
 
 # Application definition
